0af1efefba2dccf4c56e5504e1fed33641b2d170
Mii <phongsak.ysd@gmail.com>
2023-04-04 23:24:00 +0700

    image-upload app

---
+++ resources/views/livewire/image-upload.blade.php
@@ -0,0 +1,27 @@
+<div class="p-6">
+    <form wire:submit.prevent="save" class="flex flex-col w-[500px] mx-auto py-16">
+        @if ($image)
+            Preview:
+            <div class="flex flex-wrap justify-center gap-6">
+                @foreach ($image as $im)
+                    <img src="{{ $im->temporaryUrl() }}" alt="preview" class="w-[110px] h-[90px] object-cover">
+                @endforeach
+            </div>
+        @endif
+        <input type="file" wire:model="image" class="mb-4" multiple>
+
+        @error('image')
+            <span class="error">{{ $message }}</span>
+        @enderror
+
+        <button type="submit" class="py-2 px-4 bg-indigo-500 hover:bg-indigo-600 rounded text-white">
+            Save Photo
+        </button>
+    </form>
+
+    <div class="flex flex-wrap gap-7">
+        @foreach ($images as $image)
+            <img src="{{ $image }}" alt="Uploaded Image" class="w=[128px] h-[120px] object-cover">
+        @endforeach
+    </div>
+</div>
